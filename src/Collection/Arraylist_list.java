0022fb3b36052c88e9e52bad94b7d8a706e493d4
Nitin-Ratho <nitinrathod6969@gmail.com>
2025-12-03 20:53:43 +0530

    new table added

--- src/Collection/Arraylist_list.java
+++ src/Collection/Arraylist_list.java
@@ -4,18 +4,51 @@ import java.util.ArrayList;
 
 public class Arraylist_list {
 	
-	public static void main(String[] args) {
-		
+	public static void main ( String [] args ){
+	int ar1 [] = {10,50,44,55,18,22,77};
 	
-	ArrayList  ar1 = new ArrayList ();
+	int largest =Integer.MIN_VALUE;   // Min_value means it is a minimum value which int can accept. i.e -21....
+	int secondlargest =Integer.MIN_VALUE;
 	
-	ar1.add("Nitin");
-	ar1.add("Ashvin");
-	
-	for(Object arr:ar1) {
-		
-		System.out.println(arr);
+	for(int arr:ar1){
+	 
+	  if (arr >  largest){
+	  secondlargest = largest ;
+	  largest = arr;
+	  }
+	  else if ( arr > secondlargest && arr < largest ){
+	  
+	      secondlargest = arr;
+	  }
 	}
-	
+   System.out.println("Second largest value is  :- " + secondlargest);
+
 }
+	/*
+	----------------------------------------------------------------------------------------------------
+	| Iteration |  arr  |               Condition Checked                    |     Action Taken       | largest | secondlargest |
+	----------------------------------------------------------------------------------------------------
+	|     1     |  10   | 10 > -∞  (true)                                    | secondlargest = -∞     |   10    |      -∞       |
+	|           |       |                                                   | largest = 10           |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     2     |  50   | 50 > 10 (true)                                     | secondlargest = 10     |   50    |      10       |
+	|           |       |                                                   | largest = 50           |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     3     |  44   | 44 > 50 (false)                                    | secondlargest = 44     |   50    |      44       |
+	|           |       | 44 > 10 && 44 < 50 (true)                         |                        |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     4     |  55   | 55 > 50 (true)                                     | secondlargest = 50     |   55    |      50       |
+	|           |       |                                                   | largest = 55           |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     5     |  18   | 18 > 55 (false)                                    | No change              |   55    |      50       |
+	|           |       | 18 > 50 && 18 < 55 (false)                        |                        |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     6     |  22   | 22 > 55 (false)                                    | No change              |   55    |      50       |
+	|           |       | 22 > 50 && 22 < 55 (false)                        |                        |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     7     |  77   | 77 > 55 (true)                                     | secondlargest = 55     |   77    |      55       |
+	|           |       |                                                   | largest = 77           |         |               |
+	----------------------------------------------------------------------------------------------------
+	*/
+
 }
--- src/Logical_programs/ArrayLogicalprograms.java
+++ src/Logical_programs/ArrayLogicalprograms.java
@@ -88,22 +88,52 @@ public void Largest_value3rd() {
 
 public void second_Largest_value() {
   
-	int Largest =Integer.MIN_VALUE;
-	int Second_Largest =Integer.MIN_VALUE;
+int ar1 [] = {10,50,44,55,18,22,77};
 	
-	for (int arr:ar1) {
-		
-		if (arr > Largest) {
-			
-			Second_Largest = Largest ;
-			Largest = arr ;
-		}
-		else if (arr> Second_Largest && arr <Largest) {
-			Second_Largest = arr;
-		}
+	int largest =Integer.MIN_VALUE;   // Min_value means it is a minimum value which int can accept. i.e -21....
+	int secondlargest =Integer.MIN_VALUE;
+	
+	for(int arr:ar1){
+	 
+	  if (arr >  largest){
+	  secondlargest = largest ;
+	  largest = arr;
+	  }
+	  else if ( arr > secondlargest && arr < largest ){
+	  
+	      secondlargest = arr;
+	  }
 	}
-   System.out.println("Second largest valuee is :-" +Second_Largest);
+   System.out.println("Second largest value is  :- " + secondlargest);
+
 }
+	/*
+	----------------------------------------------------------------------------------------------------
+	| Iteration |  arr  |               Condition Checked                    |     Action Taken       | largest | secondlargest |
+	----------------------------------------------------------------------------------------------------
+	|     1     |  10   | 10 > -∞  (true)                                    | secondlargest = -∞     |   10    |      -∞       |
+	|           |       |                                                   | largest = 10           |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     2     |  50   | 50 > 10 (true)                                     | secondlargest = 10     |   50    |      10       |
+	|           |       |                                                   | largest = 50           |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     3     |  44   | 44 > 50 (false)                                    | secondlargest = 44     |   50    |      44       |
+	|           |       | 44 > 10 && 44 < 50 (true)                         |                        |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     4     |  55   | 55 > 50 (true)                                     | secondlargest = 50     |   55    |      50       |
+	|           |       |                                                   | largest = 55           |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     5     |  18   | 18 > 55 (false)                                    | No change              |   55    |      50       |
+	|           |       | 18 > 50 && 18 < 55 (false)                        |                        |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     6     |  22   | 22 > 55 (false)                                    | No change              |   55    |      50       |
+	|           |       | 22 > 50 && 22 < 55 (false)                        |                        |         |               |
+	----------------------------------------------------------------------------------------------------
+	|     7     |  77   | 77 > 55 (true)                                     | secondlargest = 55     |   77    |      55       |
+	|           |       |                                                   | largest = 77           |         |               |
+	----------------------------------------------------------------------------------------------------
+	*/
+
 
 //2nd way
 public void second_Largest_value2nd () {
